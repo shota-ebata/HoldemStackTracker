bde9b8b515f2fa3c96c74b4130bb8b85a6f6c399
ebata-shota <maamaaomosiroi@gmail.com>
2024-10-27 22:38:57 +0900

    DI周り編集

--- app/src/main/java/com/ebata_shota/holdemstacktracker/viewmodel/TableViewModel.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/viewmodel/TableViewModel.kt
@@ -1,12 +1,16 @@
 package com.ebata_shota.holdemstacktracker.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.ebata_shota.holdemstacktracker.domain.repository.TableStateRepository
+import com.ebata_shota.holdemstacktracker.domain.usecase.CurrentActionPlayerIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class TableViewModel(
-    private val tableStateRepo: TableStateRepository
+class TableViewModel
+@Inject
+constructor(
+//    private val tableStateRepo: TableStateRepository
+    private val currentActionPlayerIdUseCase: CurrentActionPlayerIdUseCase
 ) : ViewModel() {
-    
+
 }
