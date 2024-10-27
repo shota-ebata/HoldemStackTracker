71f65e81c8874660dfca6fecff0de3479fa6449d
ebata-shota <maamaaomosiroi@gmail.com>
2024-10-27 19:14:01 +0900

    TableStateRepository

---
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/domain/repository/TableStateRepository.kt
@@ -0,0 +1,9 @@
+package com.ebata_shota.holdemstacktracker.domain.repository
+
+import com.ebata_shota.holdemstacktracker.domain.model.TableState
+import kotlinx.coroutines.flow.Flow
+
+interface TableStateRepository {
+    fun getTableStateFlow(tableId: Long): Flow<TableState>
+    suspend fun setTableState(tableState: TableState)
+}
