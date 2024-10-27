23f6fbb83a4e495abb796501df2bf7e2cc44531c
ebata-shota <maamaaomosiroi@gmail.com>
2024-10-27 19:40:07 +0900

    isLeaved追加

--- app/src/main/java/com/ebata_shota/holdemstacktracker/domain/model/PlayerState.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/domain/model/PlayerState.kt
@@ -3,5 +3,6 @@ package com.ebata_shota.holdemstacktracker.domain.model
 data class PlayerState(
     val id: Long,
     val name: String,
-    val stack: Float
+    val stack: Float,
+    val isLeaved: Boolean
 )
