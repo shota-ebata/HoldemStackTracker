b3ee7e4fcbc27eb67da819d2b26d09e0ca17cae5
ebata-shota <maamaaomosiroi@gmail.com>
2024-11-04 20:17:37 +0900

    VMを仮設置

--- app/src/main/java/com/ebata_shota/holdemstacktracker/viewmodel/TableViewModel.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/viewmodel/TableViewModel.kt
@@ -1,15 +1,32 @@
 package com.ebata_shota.holdemstacktracker.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import com.ebata_shota.holdemstacktracker.domain.model.ActionState
 import com.ebata_shota.holdemstacktracker.domain.repository.TableStateRepository
+import com.ebata_shota.holdemstacktracker.domain.usecase.GetNextTableStateUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 @HiltViewModel
 class TableViewModel
 @Inject
 constructor(
-    private val tableStateRepo: TableStateRepository
+    private val tableStateRepo: TableStateRepository,
+    private val getNextTableStateUseCase: GetNextTableStateUseCase,
+    savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
+    private val currentTableState = tableStateRepo.getTableStateFlow(
+        tableId = TODO("savedStateから")
+    )
+
+    suspend fun setAction(action: ActionState) {
+        val updatedTableState = getNextTableStateUseCase.invoke(
+            latestTableState = currentTableState.first(),
+            action = action
+        )
+        tableStateRepo.setTableState(updatedTableState)
+    }
 }
