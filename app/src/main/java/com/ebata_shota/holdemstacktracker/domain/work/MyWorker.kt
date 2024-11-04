d272489a550f025af1bedb535d078f8392ff6b12
ebata-shota <maamaaomosiroi@gmail.com>
2024-11-04 22:08:21 +0900

    Worker(仮)

---
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/domain/work/MyWorker.kt
@@ -0,0 +1,24 @@
+package com.ebata_shota.holdemstacktracker.domain.work
+
+import android.content.Context
+import android.util.Log
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+@HiltWorker
+class MyWorker
+@AssistedInject
+constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    // TODO: DIしたいのもをここに追加
+) : CoroutineWorker(context, params) {
+    override suspend fun doWork(): Result {
+
+        Log.d("hoge", "kitaaaaa")
+        return Result.success()
+    }
+}
