32a5c33a5b923d8d84bc472132376bab0c4db80b
shota-ebata <maamaaomosiroi@gmail.com>
2024-11-17 15:36:39 +0900

    PrefRepository更新

--- app/src/main/java/com/ebata_shota/holdemstacktracker/domain/repository/PrefRepository.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/domain/repository/PrefRepository.kt
@@ -1,9 +1,40 @@
 package com.ebata_shota.holdemstacktracker.domain.repository
 
+import com.ebata_shota.holdemstacktracker.domain.model.BetViewMode
 import kotlinx.coroutines.flow.Flow
 
 
 interface PrefRepository {
     val myName: Flow<String>
     suspend fun saveMyName(myName: String)
+
+    /**
+     * Number or BB 表示
+     */
+    val defaultBetViewMode: Flow<BetViewMode>
+    suspend fun saveDefaultBetViewMode(betViewMode: BetViewMode)
+
+    /**
+     * デフォルトSBサイズ
+     */
+    val defaultSizeOfSB: Flow<Double>
+    suspend fun saveDefaultSizeOfSB(value: Double)
+
+    /**
+     * デフォルトBBサイズ
+     */
+    val defaultSizeOfBB: Flow<Double>
+    suspend fun saveDefaultSizeOfBB(value: Double)
+
+    /**
+     * デフォルトスタックサイズ（Numberモード）
+     */
+    val defaultStackSizeOfNumberMode: Flow<Double>
+    suspend fun saveDefaultStackSizeOfNumberMode(value: Double)
+
+    /**
+     * デフォルトスタックサイズ（BBモード）
+     */
+    val defaultStackSizeOfBBMode: Flow<Double>
+    suspend fun saveDefaultStackSizeOfBBMode(value: Double)
 }
--- app/src/main/java/com/ebata_shota/holdemstacktracker/infra/repository/PrefRepositoryImpl.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/infra/repository/PrefRepositoryImpl.kt
@@ -2,9 +2,16 @@ package com.ebata_shota.holdemstacktracker.infra.repository
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import com.ebata_shota.holdemstacktracker.domain.model.BetViewMode
 import com.ebata_shota.holdemstacktracker.domain.repository.PrefRepository
 import com.ebata_shota.holdemstacktracker.domain.repository.RandomIdRepository
 import com.ebata_shota.holdemstacktracker.infra.AppPreferencesKeys
+import com.ebata_shota.holdemstacktracker.infra.AppPreferencesKeys.DefaultBetViewMode
+import com.ebata_shota.holdemstacktracker.infra.AppPreferencesKeys.DefaultSizeOfBB
+import com.ebata_shota.holdemstacktracker.infra.AppPreferencesKeys.DefaultSizeOfSB
+import com.ebata_shota.holdemstacktracker.infra.AppPreferencesKeys.DefaultStackSizeOfBBMode
+import com.ebata_shota.holdemstacktracker.infra.AppPreferencesKeys.DefaultStackSizeOfNumberMode
+import com.ebata_shota.holdemstacktracker.infra.AppPreferencesKeys.MyName
 import com.ebata_shota.holdemstacktracker.infra.extension.prefFlow
 import com.ebata_shota.holdemstacktracker.infra.extension.setPrefValue
 import kotlinx.coroutines.flow.Flow
@@ -19,11 +26,57 @@ constructor(
 ) : PrefRepository {
 
     override val myName: Flow<String> by dataStore.prefFlow(
-        key = AppPreferencesKeys.MyName,
+        key = MyName,
         defaultValue = { "Player" + randomIdRepository.generateRandomId().take(6) }
     )
 
     override suspend fun saveMyName(myName: String) {
-        dataStore.setPrefValue(AppPreferencesKeys.MyName, myName)
+        dataStore.setPrefValue(MyName, myName)
+    }
+
+    override val defaultBetViewMode: Flow<BetViewMode> by dataStore.prefFlow(
+        key = DefaultBetViewMode,
+        mapToModel = { BetViewMode.entries[it] },
+        defaultValue = { BetViewMode.Number }
+    )
+
+    override suspend fun saveDefaultBetViewMode(betViewMode: BetViewMode) {
+        dataStore.setPrefValue(DefaultBetViewMode, betViewMode.ordinal)
+    }
+
+    override val defaultSizeOfSB: Flow<Double> by dataStore.prefFlow(
+        key = DefaultSizeOfSB,
+        defaultValue = { 100.0 }
+    )
+
+    override suspend fun saveDefaultSizeOfSB(value: Double) {
+        dataStore.setPrefValue(DefaultSizeOfSB, value)
+    }
+
+    override val defaultSizeOfBB: Flow<Double> by dataStore.prefFlow(
+        key = DefaultSizeOfBB,
+        defaultValue = { 200.0 }
+    )
+
+    override suspend fun saveDefaultSizeOfBB(value: Double) {
+        dataStore.setPrefValue(DefaultSizeOfBB, value)
+    }
+
+    override val defaultStackSizeOfNumberMode: Flow<Double> by dataStore.prefFlow(
+        key = DefaultStackSizeOfNumberMode,
+        defaultValue = { 10000.0 }
+    )
+
+    override suspend fun saveDefaultStackSizeOfNumberMode(value: Double) {
+        dataStore.setPrefValue(DefaultStackSizeOfNumberMode, value)
+    }
+
+    override val defaultStackSizeOfBBMode: Flow<Double> by dataStore.prefFlow(
+        key = DefaultStackSizeOfBBMode,
+        defaultValue = { 50.0 }
+    )
+
+    override suspend fun saveDefaultStackSizeOfBBMode(value: Double) {
+        dataStore.setPrefValue(DefaultStackSizeOfBBMode, value)
     }
 }
