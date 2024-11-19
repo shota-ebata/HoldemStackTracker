099962b5ef37046a7daff68c7e2baf62e9a69bb6
shota-ebata <maamaaomosiroi@gmail.com>
2024-11-19 23:19:26 +0900

    作成した拡張関数collectWithLifecycleを使う

--- app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/screen/TableCreatorScreen.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/screen/TableCreatorScreen.kt
@@ -1,12 +1,12 @@
 package com.ebata_shota.holdemstacktracker.ui.compose.screen
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ebata_shota.holdemstacktracker.domain.model.TableId
 import com.ebata_shota.holdemstacktracker.ui.compose.content.TableCreatorContent
+import com.ebata_shota.holdemstacktracker.ui.compose.extension.collectWithLifecycle
 import com.ebata_shota.holdemstacktracker.ui.viewmodel.TableCreatorUiState
 import com.ebata_shota.holdemstacktracker.ui.viewmodel.TableCreatorViewModel
 
@@ -15,10 +15,8 @@ fun TableCreatorScreen(
     viewModel: TableCreatorViewModel = hiltViewModel(),
     navigateToGame: (TableId) -> Unit
 ) {
-    LaunchedEffect(Unit) {
-        viewModel.navigateEvent.collect {
-            navigateToGame(it.tableId)
-        }
+    viewModel.navigateEvent.collectWithLifecycle {
+        navigateToGame(it.tableId)
     }
 
     val uiState: TableCreatorUiState by viewModel.uiState.collectAsStateWithLifecycle()
