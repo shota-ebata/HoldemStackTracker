cb16dea511f9a92e862bafe12f17da2ac13ee35b
shota-ebata <maamaaomosiroi@gmail.com>
2024-12-10 22:20:44 +0900

    スクロールでFABを隠す

--- app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/content/MainContent.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/content/MainContent.kt
@@ -1,34 +1,35 @@
 package com.ebata_shota.holdemstacktracker.ui.compose.content
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Card
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ebata_shota.holdemstacktracker.R
 import com.ebata_shota.holdemstacktracker.domain.model.TableId
-import com.ebata_shota.holdemstacktracker.domain.model.TableSummary
 import com.ebata_shota.holdemstacktracker.ui.compose.row.TableSummaryCardRow
 import com.ebata_shota.holdemstacktracker.ui.compose.row.TableSummaryCardRowUiState
-import com.ebata_shota.holdemstacktracker.ui.theme.CardSideSpace
+import com.ebata_shota.holdemstacktracker.ui.extension.isScrollingUp
 import com.ebata_shota.holdemstacktracker.ui.theme.HoldemStackTrackerTheme
-import java.time.LocalDateTime
 
 @Composable
 fun MainContent(
@@ -38,32 +39,37 @@ fun MainContent(
     onClickQrScan: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val listState: LazyListState = rememberLazyListState()
 
     Scaffold(
         modifier = modifier,
         floatingActionButton = {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+            AnimatedVisibility(
+                visible = listState.isScrollingUp().value
             ) {
-                FloatingActionButton(
-                    onClick = {
-                        onClickQrScan()
-                    },
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
                 ) {
-                    Icon(
-                        painter = painterResource(R.drawable.baseline_qr_code_scanner_24),
-                        contentDescription = "QR Scanner"
-                    )
-                }
-                FloatingActionButton(
-                    onClick = {
-                        onClickFloatingButton()
-                    },
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.Add,
-                        contentDescription = "Add"
-                    )
+                    FloatingActionButton(
+                        onClick = {
+                            onClickQrScan()
+                        },
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.baseline_qr_code_scanner_24),
+                            contentDescription = "QR Scanner"
+                        )
+                    }
+                    FloatingActionButton(
+                        onClick = {
+                            onClickFloatingButton()
+                        },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "Add"
+                        )
+                    }
                 }
             }
         },
@@ -77,7 +83,8 @@ fun MainContent(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            state = listState
         ) {
             items(
                 items = uiState.tableSummaryList
---
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/ui/extension/LazyListStateExtension.kt
@@ -0,0 +1,29 @@
+package com.ebata_shota.holdemstacktracker.ui.extension
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.snapshotFlow
+
+/**
+ * 参考：
+ * https://stackoverflow.com/questions/74381573/jetpack-compose-fab-hide-on-scroll-behavior
+ */
+@Composable
+fun LazyListState.isScrollingUp(): State<Boolean> {
+    return produceState(initialValue = true) {
+        var lastIndex = 0
+        var lastScroll = Int.MAX_VALUE
+        snapshotFlow {
+            firstVisibleItemIndex to firstVisibleItemScrollOffset
+        }.collect { (currentIndex, currentScroll) ->
+            if (currentIndex != lastIndex || currentScroll != lastScroll) {
+                value = currentIndex < lastIndex ||
+                        (currentIndex == lastIndex && currentScroll < lastScroll)
+                lastIndex = currentIndex
+                lastScroll = currentScroll
+            }
+        }
+    }
+}
