9320d583761cad52afe318df9cfdc3e8736cb437
shota-ebata <maamaaomosiroi@gmail.com>
2024-12-14 00:22:33 +0900

    Tableの購読を終了するときに、nullを入れる。

--- app/src/main/java/com/ebata_shota/holdemstacktracker/infra/repository/TableRepositoryImpl.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/infra/repository/TableRepositoryImpl.kt
@@ -30,6 +30,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.time.Instant
@@ -122,6 +123,7 @@ constructor(
     }
 
     override fun stopCollectTableFlow() {
+        _tableStateFlow.update { null }
         collectTableJob?.cancel()
         collectTableJob = null
         currentTableId = null
