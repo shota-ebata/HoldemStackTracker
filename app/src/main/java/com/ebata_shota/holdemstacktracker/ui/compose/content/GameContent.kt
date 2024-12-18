1c2f2d816e9a6bc8d7eb77849291a2a409d2b73f
shota-ebata <maamaaomosiroi@gmail.com>
2024-12-19 00:34:03 +0900

    プレビューのデバイスを変更

--- app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/content/GameContent.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/content/GameContent.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -192,16 +193,16 @@ private class GameContentUiStatePreviewParam :
 }
 
 @Preview(
-    widthDp = 480,
     showBackground = true,
     showSystemUi = true,
+    device = Devices.PIXEL_6A,
     name = "Light Mode"
 )
 @Preview(
-    widthDp = 480,
     showBackground = true,
     showSystemUi = true,
     uiMode = Configuration.UI_MODE_NIGHT_YES,
+    device = Devices.PIXEL_6A,
     name = "Dark Mode"
 )
 @Composable
