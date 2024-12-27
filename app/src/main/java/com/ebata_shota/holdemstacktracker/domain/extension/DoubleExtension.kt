0a6c56af094a0b7ddf46b6bdcae108def1b1eb44
shota-ebata <maamaaomosiroi@gmail.com>
2024-12-28 02:08:41 +0900

    桁区切り

--- app/src/main/java/com/ebata_shota/holdemstacktracker/domain/extension/DoubleExtension.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/domain/extension/DoubleExtension.kt
@@ -4,7 +4,7 @@ import com.ebata_shota.holdemstacktracker.domain.model.BetViewMode
 
 fun Double.toHstString(betViewMode: BetViewMode): String {
     return when (betViewMode) {
-        BetViewMode.Number -> this.toInt().toString() // TODO: 四捨五入したい
+        BetViewMode.Number -> "%,d".format(this.toInt())
         BetViewMode.BB -> this.toString()// TODO: 小数点第一位で丸めたい
     }
 }
