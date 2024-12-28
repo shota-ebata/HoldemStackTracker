521422c09b5923d1c61a7909a0cf70090d2de2c9
shota-ebata <maamaaomosiroi@gmail.com>
2024-12-28 22:41:52 +0900

    RaiseSize変更ボタン

---
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/parts/RaiseSizeChangeButton.kt
@@ -0,0 +1,60 @@
+package com.ebata_shota.holdemstacktracker.ui.compose.parts
+
+import android.content.res.Configuration
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ebata_shota.holdemstacktracker.R
+import com.ebata_shota.holdemstacktracker.domain.model.StringSource
+import com.ebata_shota.holdemstacktracker.ui.theme.HoldemStackTrackerTheme
+
+@Composable
+fun RaiseSizeChangeButton(
+    uiState: RaiseSizeChangeButtonUiState,
+    onClickRaiseSizeButton: (Double) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedButton(
+        modifier = modifier,
+//        shape = RoundedCornerShape(8.dp),
+        onClick = { onClickRaiseSizeButton(uiState.raiseSize) }
+    ) {
+        Text(
+            text = uiState.labelStringSource.getString(),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+data class RaiseSizeChangeButtonUiState(
+    val labelStringSource: StringSource,
+    val raiseSize: Double,
+)
+
+@Preview(showBackground = true, showSystemUi = false, name = "Light Mode")
+@Preview(
+    showBackground = true,
+    showSystemUi = false,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark Mode"
+)
+@Composable
+private fun RaiseSizeChangeButtonPreview() {
+    HoldemStackTrackerTheme {
+        Surface {
+            RaiseSizeChangeButton(
+                uiState = RaiseSizeChangeButtonUiState(
+                    labelStringSource = StringSource(id = R.string.position_label_sb),
+                    raiseSize = 4.0
+                ),
+                onClickRaiseSizeButton = {},
+            )
+        }
+    }
+}
