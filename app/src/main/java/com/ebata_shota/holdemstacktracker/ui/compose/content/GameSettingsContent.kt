299543a27e88d061673a0229fc203c68ea5774d7
shota-ebata <maamaaomosiroi@gmail.com>
2025-01-03 16:38:20 +0900

    設定画面のアイコン表示

--- app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/content/GameSettingsContent.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/ui/compose/content/GameSettingsContent.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,6 +31,12 @@ fun GameSettingsContent(
             .verticalScroll(rememberScrollState)
     ) {
         ListItem(
+            leadingContent = {
+                Icon(
+                    painter = painterResource(R.drawable.backlight_high_24),
+                    contentDescription = null
+                )
+            },
             headlineContent = {
                 Text(
                     text = stringResource(R.string.setting_keep_screen_on),
