36ba9aef1cc65a37945df159fafa37189cc15b7e
shota-ebata <maamaaomosiroi@gmail.com>
2025-02-24 00:38:00 +0900

    テストに理想を記載

--- app/src/test/java/com/ebata_shota/holdemstacktracker/domain/usecase/GetPotListUseCaseImplTest.kt
+++ app/src/test/java/com/ebata_shota/holdemstacktracker/domain/usecase/GetPotListUseCaseImplTest.kt
@@ -277,8 +277,17 @@ class GetPotListUseCaseImplTest {
         )
     }
 
+    /**
+     * 0 Bet    200
+     * 1 Call   200
+     * 2 Raise  400
+     * 0 Call   400
+     * 1 Fold
+     *
+     * この時Potは1000 でcloseしない
+     */
     @Test
-    fun fold() {
+    fun fold_1_call_2() {
         val updatedPlayers = listOf(
             GamePlayer(
                 id = PlayerId("0"),
@@ -306,24 +315,14 @@ class GetPotListUseCaseImplTest {
             Pot(
                 id = PotId("0"),
                 potNumber = 0,
-                potSize = 600,
+                potSize = 1000,
                 involvedPlayerIds = listOf(
                     PlayerId("0"),
                     PlayerId("1"),
                     PlayerId("2"),
                 ),
-                isClosed = true
-            ),
-            Pot(
-                id = PotId("0"),
-                potNumber = 1,
-                potSize = 400,
-                involvedPlayerIds = listOf(
-                    PlayerId("0"),
-                    PlayerId("2"),
-                ),
                 isClosed = false
-            ),
+            )
         )
         executeAndAssert(
             updatedPlayers = updatedPlayers,
@@ -492,6 +491,15 @@ class GetPotListUseCaseImplTest {
     /**
      * 1人AllIn
      * 1人Fold
+     *
+     * 0 Blind  1
+     * 1 Blind  2
+     * 0 AllIn  101
+     * 1 Fold
+     *
+     * この時
+     * Pot    4
+     * あまり  99
      */
     @Test
     fun all_in_1_fold_1() {
@@ -540,6 +548,10 @@ class GetPotListUseCaseImplTest {
     /**
      * 1人AllIn
      * 1人AllIn
+     *
+     * 差額のある2人がAllInした場合
+     * Pot    200
+     * あまり  1
      */
     @Test
     fun all_in_2() {
@@ -581,6 +593,140 @@ class GetPotListUseCaseImplTest {
                 potList = expectedList,
                 pendingBetPerPlayerWithoutZero = mapOf(
                     PlayerId("0") to 1
+                )
+            ),
+        )
+    }
+
+    /**
+     * オール・インした人よりも低いBetをした人がFold
+     *
+     * 2 Bet    10
+     * 0 AllIn  12
+     * 1 AllIn  200
+     * 2 Fold
+     *
+     * Pot    34
+     * あまり  188
+     */
+    @Test
+    fun fold_under_all_in() {
+        val updatedPlayers = listOf(
+            GamePlayer(
+                id = PlayerId("0"),
+                stack = 0,
+            ),
+            GamePlayer(
+                id = PlayerId("1"),
+                stack = 0,
+            ),
+            GamePlayer(
+                id = PlayerId("2"),
+                stack = 100,
+            ),
+        )
+        val pendingBetPerPlayerWithoutZero: Map<PlayerId, Int> = mapOf(
+            PlayerId("0") to 12,
+            PlayerId("1") to 200,
+            PlayerId("2") to 10,
+        )
+        val activePlayerIds = listOf(
+            PlayerId("0"),
+            PlayerId("1"),
+        )
+        val expectedList = listOf(
+            Pot(
+                id = PotId("0"),
+                potNumber = 0,
+                potSize = 34,
+                involvedPlayerIds = listOf(
+                    PlayerId("0"),
+                    PlayerId("1"),
+                    PlayerId("2"),
+                ),
+                isClosed = true
+            )
+        )
+        executeAndAssert(
+            updatedPlayers = updatedPlayers,
+            pendingBetPerPlayerWithoutZero = pendingBetPerPlayerWithoutZero,
+            activePlayerIds = activePlayerIds,
+            expected = PotAndRemainingBet(
+                potList = expectedList,
+                pendingBetPerPlayerWithoutZero = mapOf(
+                    PlayerId("1") to 188
+                )
+            ),
+        )
+    }
+
+    /**
+     * オール・インを超えたBetをした人がFoldした場合
+     *
+     * 2 Bet    12
+     * 0 AllIn  10
+     * 1 AllIn  200
+     * 2 Fold
+     *
+     * Pot  34
+     * あまり 188
+     */
+    @Test
+    fun fold_over_all_in() {
+        val updatedPlayers = listOf(
+            GamePlayer(
+                id = PlayerId("0"),
+                stack = 0,
+            ),
+            GamePlayer(
+                id = PlayerId("1"),
+                stack = 0,
+            ),
+            GamePlayer(
+                id = PlayerId("2"),
+                stack = 100,
+            ),
+        )
+        val pendingBetPerPlayerWithoutZero: Map<PlayerId, Int> = mapOf(
+            PlayerId("0") to 10,
+            PlayerId("1") to 200,
+            PlayerId("2") to 12,
+        )
+        val activePlayerIds = listOf(
+            PlayerId("0"),
+            PlayerId("1"),
+        )
+        val expectedList = listOf(
+            Pot(
+                id = PotId("0"),
+                potNumber = 0,
+                potSize = 30,
+                involvedPlayerIds = listOf(
+                    PlayerId("0"),
+                    PlayerId("1"),
+                    PlayerId("2"),
+                ),
+                isClosed = true
+            ),
+            Pot(
+                id = PotId("0"),
+                potNumber = 1,
+                potSize = 4,
+                involvedPlayerIds = listOf(
+                    PlayerId("1"),
+                    PlayerId("2"),
+                ),
+                isClosed = false
+            ),
+        )
+        executeAndAssert(
+            updatedPlayers = updatedPlayers,
+            pendingBetPerPlayerWithoutZero = pendingBetPerPlayerWithoutZero,
+            activePlayerIds = activePlayerIds,
+            expected = PotAndRemainingBet(
+                potList = expectedList,
+                pendingBetPerPlayerWithoutZero = mapOf(
+                    PlayerId("1") to 188
                 )
             ),
         )
