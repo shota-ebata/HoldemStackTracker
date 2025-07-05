14eeb61309f4d5cf83a5c6c2eac622f8e4c04d11
shota-ebata <maamaaomosiroi@gmail.com>
2025-07-05 12:31:19 +0900

    withContextAndExceptionWrapperにcoroutineNameを必須化

--- app/src/main/java/com/ebata_shota/holdemstacktracker/domain/util/CoroutineUtil.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/domain/util/CoroutineUtil.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 /**
  * withContextを使用して、例外をラップするための関数
@@ -21,23 +20,36 @@ import kotlin.coroutines.coroutineContext
  */
 suspend fun <T> withContextAndExceptionWrapper(
     context: CoroutineContext,
+    coroutineName: String,
     block: suspend CoroutineScope.() -> T,
 ): T {
     // 呼び出し元のメソッド名と行番号を取得
-    // ただし、難読化されている場合は、ファイル名や行番号を取得できない可能性がある。
-    val callerStackTraceElement = Throwable().stackTrace[1] // 呼び出しているメソッド
-    val callerOfCallerStackTraceElement = Throwable().stackTrace[2] // 呼び出しているメソッドの更に呼び出し元
-    var callerInfo =
-        "(${callerOfCallerStackTraceElement.fileName}:${callerOfCallerStackTraceElement.lineNumber}→${callerStackTraceElement.fileName}:${callerStackTraceElement.lineNumber})"
-    return try {
-        withContext(context) {
-            block()
+    // ただし、難読化されている場合は、ファイル名や行番号を取得できない可能性がある。ので不採用
+//    val callerStackTraceElement = Throwable().stackTrace[1] // 呼び出しているメソッド
+//    val callerOfCallerStackTraceElement = Throwable().stackTrace[2] // 呼び出しているメソッドの更に呼び出し元
+//    var callerInfo =
+//        "(${callerOfCallerStackTraceElement.fileName}:${callerOfCallerStackTraceElement.lineNumber}→${callerStackTraceElement.fileName}:${callerStackTraceElement.lineNumber})"
+    return withContext(CoroutineName(coroutineName)) {
+        try {
+            withContext(context) {
+                block()
+            }
+        } catch (e: CancellationException) {
+            // CancellationExceptionはそのまま投げる(suspend中断例外の伝播のため)
+            throw e
+        } catch (e: Exception) {
+            val callerInfo = coroutineContext[CoroutineName]?.name!!
+            throw WrappedException(callerInfo, e)
         }
-    } catch (e: CancellationException) {
-        // CancellationExceptionはそのまま投げる(suspend中断例外の伝播のため)
-        throw e
-    } catch (e: Exception) {
-        callerInfo = coroutineContext[CoroutineName]?.name ?: callerInfo
-        throw WrappedException(callerInfo, e)
     }
+}
+
+suspend fun <T> withContext(
+    coroutineName: String,
+    block: suspend CoroutineScope.() -> T,
+): T {
+    return withContext(
+        context = CoroutineName(name = coroutineName),
+        block = block
+    )
 }
