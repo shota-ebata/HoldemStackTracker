449e0475498f9cc555fa495f2ab7db34214c5584
shota-ebata <maamaaomosiroi@gmail.com>
2026-01-23 22:10:12 +0900

    Pot精算処理のリファクタリング
    - `SetPotSettlementInfoUseCaseImpl`内の処理をprivate funに分割
    - 処理の意図が分かりやすくなるようにコメントを追加
    - 使用していない`GetNextPhaseUseCase`の依存を削除

--- app/src/main/java/com/ebata_shota/holdemstacktracker/domain/usecase/impl/SetPotSettlementInfoUseCaseImpl.kt
+++ app/src/main/java/com/ebata_shota/holdemstacktracker/domain/usecase/impl/SetPotSettlementInfoUseCaseImpl.kt
@@ -10,7 +10,6 @@ import com.ebata_shota.holdemstacktracker.domain.model.PlayerId
 import com.ebata_shota.holdemstacktracker.domain.model.PotSettlementInfo
 import com.ebata_shota.holdemstacktracker.domain.repository.GameRepository
 import com.ebata_shota.holdemstacktracker.domain.repository.RandomIdRepository
-import com.ebata_shota.holdemstacktracker.domain.usecase.GetNextPhaseUseCase
 import com.ebata_shota.holdemstacktracker.domain.usecase.GetNotFoldPlayerIdsUseCase
 import com.ebata_shota.holdemstacktracker.domain.usecase.SetPotSettlementInfoUseCase
 import com.ebata_shota.holdemstacktracker.domain.util.getSortedByActionOrder
@@ -36,14 +35,19 @@ constructor(
         game: Game,
         pots: List<PotSettlementDialogUiState.PotUiState>,
     ) = withContext(dispatcher) {
+        // アクション順にソートされたプレイヤーリストを取得
         val playerOrder = game.playerOrder
         val actionOrder = playerOrder.getSortedByActionOrder(btnPlayerId = game.btnPlayerId)
+        // フォールドしていないプレイヤーIDのリストを取得
         val notFoldPlayerIds = notFoldPlayerIds.invoke(playerOrder = playerOrder, game.phaseList)
+        // SBに一番近いプレイヤー（アクションが一番早いプレイヤー）を取得
         val nearestSbPlayerId = actionOrder.first { playerId ->
             notFoldPlayerIds.any { it == playerId }
         }
+        // Potの精算情報を作成
         val potSettlementInfoList: List<PotSettlementInfo> = game.potList.map { pot ->
             val potUiState = pots.find { it.potId == pot.id }!!
+            // そのPotを獲得したプレイヤーのIDリスト
             val selectedPlayerIds: List<PlayerId> = potUiState.players
                 .filter { it.isSelected }
                 .map { it.playerId }
@@ -55,24 +59,32 @@ constructor(
             )
         }
 
+        // Pot獲得後の各プレイヤーの状態を計算
         val gamePlayers = getNewGamePlayers(
             players = game.players,
             nearestSbPlayerId = nearestSbPlayerId,
             potSettlementInfoList = potSettlementInfoList,
         )
 
+        // ゲーム終了のフェーズを作成
         val nextPhase = getEndPhase(potSettlementInfoList)
 
+        // Repositoryにゲーム情報を送信
         gameRepository.sendGame(
             tableId = game.tableId,
             newGame = game.copy(
                 players = gamePlayers,
                 phaseList = game.phaseList + nextPhase,
-                potList = emptyList()
+                potList = emptyList() // Potをリセット
             ),
         )
     }
 
+    /**
+     * ゲーム終了フェーズを作成する
+     * @param potSettlementInfoList Potの精算情報
+     * @return [Phase.End]
+     */
     private fun getEndPhase(
         potSettlementInfoList: List<PotSettlementInfo>
     ): Phase.End {
@@ -81,17 +93,24 @@ constructor(
             gameResult = GameResult(
                 potResults = potSettlementInfoList
                     .mapIndexed { index, potSettlementInfo ->
-                    GameResult.PotResult(
-                        id = potSettlementInfo.potId,
-                        potNumber = potSettlementInfo.potNumber,
-                        potSize = potSettlementInfo.potSize,
-                        winnerPlayerIds = potSettlementInfo.acquirerPlayerIds
-                    )
-                }
+                        GameResult.PotResult(
+                            id = potSettlementInfo.potId,
+                            potNumber = potSettlementInfo.potNumber,
+                            potSize = potSettlementInfo.potSize,
+                            winnerPlayerIds = potSettlementInfo.acquirerPlayerIds
+                        )
+                    }
             ),
         )
     }
 
+    /**
+     * Pot獲得後の各プレイヤーの状態を計算する
+     * @param players 現在のプレイヤーリスト
+     * @param potSettlementInfoList Potの精算情報
+     * @param nearestSbPlayerId SBに一番近いプレイヤーのID
+     * @return [List<GamePlayer>]
+     */
     private fun getNewGamePlayers(
         players: List<GamePlayer>,
         potSettlementInfoList: List<PotSettlementInfo>,
@@ -101,7 +120,9 @@ constructor(
         potSettlementInfoList.forEach { potSettlementInfo ->
             val potSize = potSettlementInfo.potSize
             val acquirerPlayerSize = potSettlementInfo.acquirerPlayerIds.size
+            // Potを山分けした際の余り
             val mod: Int = potSize % acquirerPlayerSize
+            // Potを山分けした際の各プレイヤーの獲得額
             val acquireSize = (potSize - mod) / acquirerPlayerSize
             gamePlayers = gamePlayers.map { gamePlayer ->
                 if (potSettlementInfo.acquirerPlayerIds.any { it == gamePlayer.id }) {
